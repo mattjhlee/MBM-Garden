6238e89203e5bae803e7dabbdeacc2d9d38ba440
Macolister <bispo.mac@gmail.com>
2023-06-29 22:11:01 -0400

    test to see my portion

--- app/cli.py
+++ app/cli.py
@@ -2,7 +2,9 @@ from app import app
 from models import db, Plant, Garden, Gardener
 from helpers import (
     add_gardener,
-    view_gardens
+    view_gardens,
+    view_plants,
+    menu_selection
 )
 
 if __name__ == "__main__":
@@ -25,15 +27,19 @@ if __name__ == "__main__":
         gardeners = db.session.query(Gardener)
         gardens = db.session.query(Garden)
 
-        gardener_name = input("Name: ")
-        name_check = False
-        for gardener in gardeners:
-            if gardener.name.lower() == gardener_name.lower():
-                print(f"Welcome back, {gardener.name}.")
-                name_check = True
-        if name_check == False:
-            add_gardener(gardener_name)
+        # view_plants(plants)
+        menu_selection(gardens, plants, gardeners)
 
-        print("Here is a list of our current gardens:")
-        view_gardens(gardens, plants, gardeners)    
+        # gardener_name = input("Name: ")
+        # name_check = False
+        # for gardener in gardeners:
+        #     if gardener.name.lower() == gardener_name.lower():
+        #         print(f"Welcome back, {gardener.name}.")
+        #         name_check = True
+        # if name_check == False:
+        #     add_gardener(gardener_name)
+
+        # print("Here is a list of our current gardens:")
+        # view_gardens(gardens, plants, gardeners)    
+        
         
